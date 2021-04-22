589477a8bf8894af09197356b9c66dcde967291b
titustortoiseturtle1999 <55660974+titustortoiseturtle1999@users.noreply.github.com>
2021-04-22 21:18:21 +0800

    Update nav_launcher.py

--- nav_launcher.py
+++ nav_launcher.py
@@ -24,9 +24,7 @@ import adafruit_amg88xx
 
 pi = pigpio.pi()
 
-
 #setup for launcher & rack
-
 en = 21
 in2 = 5
 in1 = 6
@@ -34,7 +32,7 @@ d2 = 19
 d1 = 13
 sf = 16
 
-#pitch control #edit values
+#pitch control 
 forwardp = 2000
 backp = 1000
 stopp = 1500
@@ -58,7 +56,6 @@ pi.write(d1, 0)
 THRESHOLDTEMP = 35
 
 # setup i2c for ir
-
 ir_power_pin = 17
 pi.write(ir_power_pin, 1)
 time.sleep(1)
@@ -69,33 +66,29 @@ amg = adafruit_amg88xx.AMG88XX(i2c)
 class MinimalPublisher(Node):
     def __init__(self):
         super().__init__('minimal_publisher')
-
         # create publisher for data about target
         self.publisher_ = self.create_publisher(String, 'topic', 10)
         timer_period = 0.1 # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
 
-        # create subscriber to tell launcher when it can fire
-        self.launcher_ready_subscription = self.create_subscription(
-            String,
-            'launcher_ready',
-            self.launcher_ready_callback,
-            10)
-        self.launcher_ready_subscription # prevent unused variable warning
-        self.ready = False
-        self.target_hit = False
-
-    def launcher_ready_callback(self, msg):
-        self.get_logger().info('Received fr launcher_ready: "%s"' % msg.data)
-        if (msg.data == 'launcher ready'):
-            self.ready = True
-        else:
-            self.ready = False
-
     def timer_callback(self):
+        if (not self.target_hit):
+            data = String()
+            data = self.detect_target()
+            msg = String()
+            msg.data = data
+            self.publisher_.publish(msg)
+            self.get_logger().info('Publishing: "%s"' % msg.data)
+            if (data == 'on target'):
+                self.fire_ping_pong()
+        return
+    
+    # identify if there is a target and where it is
+    def detect_target(self):
        i = 0
         max_temp = 0
         combined_row = [0 for i in range(8)]
+        # store the vertical weights 
         for row in amg.pixels:
             for temp in row:
         #        print(['{0:.1f}'.format(temp)], end = " ")
@@ -105,8 +98,9 @@ class MinimalPublisher(Node):
          #   print()
             i += 1
         #print(combined_row)
-        #detect target
+        # compare the vertical weights to identify the target
         weight_left = (combined_row[0] + combined_row[1] + combined_row[2])
+        # since we do not stop the bot, we try to fire as soon as the target is in our field of view
         weight_center = (combined_row[3] + combined_row[4] + combined_row[5] + combined_row[6] + combined_row[7]) *3/5
         weight_right = 0
         if (self.target_hit):
@@ -124,45 +118,24 @@ class MinimalPublisher(Node):
 
     def fire_ping_pong(self):
         if (not self.target_hit):
-            # aim picthing
-            #pitchcontrols
-            combined_col = [0 for i in range(8)]
-            middle = False;
-            count = 0
-            count_up = 0
-            count_down = 0
-            """
-            while(not middle and count < 5):
-                max_temp = 0
-                for row in amg.pixels:
-                    col_i = 0;
-                    for temp in row:
-                        print(['{0:.1f}'.format(temp)], end = " ")
-                        combined_col[col_i] += temp
-                        if (temp > max_temp):
-                            max_temp = temp
-                        col_i += 1;
-                    print()
-            """
             pi.write(en,1)                   # start flywheels
             for i in range (3):
-                pi.set_servo_pulsewidth(servo_pin, backward)      # move rack back
+                pi.set_servo_pulsewidth(servo_pin, backward)     # move rack back
                 time.sleep(0.21)
                 pi.set_servo_pulsewidth(servo_pin, stop)         # stop rack movement
                 time.sleep(0.1)
-                pi.set_servo_pulsewidth(servo_pin, forward) # move rack forward to push ball
+                pi.set_servo_pulsewidth(servo_pin, forward)      # move rack forward to push ball
                 time.sleep(0.23)
                 pi.set_servo_pulsewidth(servo_pin, stop)         # stop rack movement
                 time.sleep(0.1)
-                # tilt launcher up
-                pi.set_servo_pulsewidth(pitchpin, forwardp)
+                pi.set_servo_pulsewidth(pitchpin, forwardp)      # tilt launcher up
                 time.sleep(0.04)
                 pi.set_servo_pulsewidth(pitchpin, stopp)
                 time.sleep(0.1)
             pi.write(en, 0)                  # stop flywheels
         self.target_hit = True
 
-        pi.set_servo_pulsewidth(pitchpin, backp)
+        pi.set_servo_pulsewidth(pitchpin, backp)                 # reset launcher
         time.sleep(0.14)
         pi.set_servo_pulsewidth(pitchpin, stopp)
         time.sleep(0.4)
@@ -182,8 +155,6 @@ def main(args=None):
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
-        #GPIO.output(en,0)
-        #GPIO.cleanup()
         minimal_publisher.destroy_node()
         rclpy.shutdown()
 
